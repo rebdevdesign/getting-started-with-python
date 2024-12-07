11129a7af8224769c5e2834c5b0adf67920b2805
Rebecca Morris <rebthemaker@gmail.com>
2024-12-07 12:19:23 -0600

    My birthday reminder app

--- build-a-birthday-reminder-app/main.py
+++ build-a-birthday-reminder-app/main.py
@@ -1,1 +1,9 @@
 user_input = input("Which birthday are you looking for? ")
+if user_input == "Jennifer":
+    print("Birthday is 06/05/77!")
+elif user_input == "Kennedy":
+    print( "Birthday is 06/19/94!")
+elif user_input == "Barbara":
+    print("Birthday is 01/25/47!")
+else:
+    print("Can't find birthday for this person...")
